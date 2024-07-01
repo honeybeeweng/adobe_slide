246a5a38adef706402c8ca23b793a47b5f1a198f
honeybeeweng <kelly14039@hotmail.com>
2024-07-01 20:25:19 +0800

    24/07/01 上傳
    24/07/01 上傳

--- js/master.js
+++ js/master.js
@@ -59,37 +59,6 @@ function setScroll(){
     });
 }
 
-// 06/19寫法
-// function setScroll(){
-//     $(window).scroll(function(){
-//         if (isMobile == true) {
-//             // console.log($(window).scrollTop());
-//             if ($(window).scrollTop() == 0) {
-//                 $nav.removeClass('nav-active');
-                
-//                 // console.log(0)
-                
-//             }else {
-//                 $nav.addClass('nav-active');
-                
-//                 // console.log('要套用 class')
-//             }
-//         }
-//     });
-// }
-
-
-// 其他動畫載入的函式庫
-function setInit() {
-    setGrid();
-}
-
-// 其他事件的函式庫
-function setEvent() {
-    setScroll();
-    setFancybox();
-}
-
 function setFancybox(){
     $grid.find('a').fancybox({
         protect: true,
@@ -127,6 +96,97 @@ function setFancybox(){
             }
         }
     });
+}
+
+function setClickBtn() {
+    console.log($nav.find('.nav-btn'));
+    const $btns = $nav.find('.nav-btn');
+    $btns.click(function() {
+        $(this)
+            .attr('disabled', true)
+            .siblings().attr('disabled', false);
+        const index = $(this).index();
+        // console.log(index);
+        
+        // if(index == 0) {
+        //     // console.log('我是0')
+        //     $slide.css('transform', 'translate(0, 0)');
+        // // } else {
+        // //     console.log('我不是0') 
+        // }
+
+        // if(index == 1) {
+        //     $slide.css('transform', 'translate(-100vw, 0)');
+        // }
+
+        // if(index == 2) {
+        //     $slide.css('transform', 'translate(-200vw, 0)');
+        // }
+
+        // if(index == 3) {
+        //     $slide.css('transform', 'translate(0, -100vh)');
+        // }
+
+        // if(index == 4) {
+        //     $slide.css('transform', 'translate(-100vw, -100vh)');
+        // }
+
+        // if(index == 5) {
+        //     $slide.css('transform', 'translate(-200vw, -100vh)');
+        // }
+
+        // 上述第二種寫法，六選一(優點:不用逐一跑程式)
+        // if(index == 0) {
+        //     $slide.css('transform', 'translate(0, 0)');
+        // } else if(index == 1) {
+        //     $slide.css('transform', 'translate(-100vw, 0)');
+        // } else if(index == 2) {
+        //     $slide.css('transform', 'translate(-200vw, 0)');
+        // } else if(index == 3) {
+        //     $slide.css('transform', 'translate(0, -100vh)');
+        // } else if(index == 4) {
+        //     $slide.css('transform', 'translate(-100vw, -100vh)');
+        // } else if(index == 5) {
+        //     $slide.css('transform', 'translate(-200vw, -100vh)');
+        // }
+
+        // 第三種寫法
+        switch(index) {
+            case 0:
+                $slide.css('transform', 'translate(0, 0)');
+                break;
+            case 1:
+                $slide.css('transform', 'translate(-100vw, 0)');
+                break;
+            case 2:
+                $slide.css('transform', 'translate(-200vw, 0)');
+                break;
+            case 3:
+                $slide.css('transform', 'translate(0, -100vh)');
+                break;
+            case 4:
+                $slide.css('transform', 'translate(-100vw, -100vh)');
+                break;
+            case 5:
+                $slide.css('transform', 'translate(-200vw, -100vh)');
+                break;
+        }
+
+
+    });
+}
+
+// 其他動畫載入的函式庫
+function setInit() {
+    setGrid();
+    $nav.find('.nav-btn').eq(0).attr('disabled', true);
+}
+
+// 其他事件的函式庫
+function setEvent() {
+    setScroll();
+    setFancybox();
+    setClickBtn();
 }
 
 setInit();
